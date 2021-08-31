69bb54a00ae671b1776083c1f94a4232c65effc6
yagnurl <50737107+yagnurl@users.noreply.github.com>
2021-08-31 15:56:58 +0300

    adding item functions added

--- script.js
+++ script.js
@@ -98,6 +98,32 @@ function updateDOM() {
 
 }
 
+//Add to Column List, Reset Textbix
+function addToColumn(column) {
+  const itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = '';
+  updateDOM();
+}
+
+//Show Add Item Input Box
+function showInputBox(column) {
+  addBtns[column].style.visibility = 'hidden';
+  saveItemBtns[column].style.display = 'flex';
+  addItemContainers[column].style.display = 'flex';
+}
+
+//Hide Item Input Box
+function hideInputBox(column) {
+  addBtns[column].style.visibility = 'visible';
+  saveItemBtns[column].style.display = 'none';
+  addItemContainers[column].style.display = 'none';
+  addToColumn(column);
+}
+
+
+
 //Alloow arrays to reflect Drag and Drop Items
 function rebuildArrays() {
 
